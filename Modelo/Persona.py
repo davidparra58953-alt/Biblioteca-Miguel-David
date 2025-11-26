b83c49774afe7328ac7e604c7371d4d280892ebd
David <david.parra58953@ucaldas.edu.co>
2025-11-26 17:06:00 -0500

    persona

---
+++ Modelo/Persona.py
@@ -0,0 +1,12 @@
+class Persona():
+    """
+    Representa el objeto persona:
+
+    Atributos:
+    name(String):
+    ID(Int):
+
+    """
+    def __init__(self,name,ID):
+        self.__name=name
+        self.__ID=ID
